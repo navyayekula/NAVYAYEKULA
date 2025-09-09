c3eb66d556e508a560c47a6277e85e9d823a2c06
nav <324103211063.navya@gvpcew.ac.in>
2025-09-09 10:24:41 +0530

    hello

---
+++ add.java
@@ -0,0 +1,9 @@
+public class Add
+{
+public static void main(String[] args)
+{
+int a=20;
+int b=7;
+System.out.println("Addition: "+(a+b));
+}
+}
---
+++ test1.html
@@ -0,0 +1,5 @@
+<html>
+<head>
+<title>Welcome to HTML World</title>
+</head>
+</html>
